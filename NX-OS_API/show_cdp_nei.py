02b755e0ccfae3180947de732d6a87f5de6581b5
Tory Davenport <contact@torydavenport.com>
2020-07-23 15:09:14 -0400

    Adding new projects

--- NX-OS_API/show_cdp_nei.py
+++ NX-OS_API/show_cdp_nei.py
@@ -2,14 +2,26 @@ import requests
 
 url = "http://192.168.0.253:80/ins"
 
-payload = "{\n  \"ins_api\": {\n    \"version\": \"1.2\",\n    \"type\": \"cli_show\",\n    \"chunk\": \"0\",\n    \"sid\": \"1\",\n    \"input\": \"show cdp nei \",\n    \"output_format\": \"json\"\n  }\n}"
+payload = "{
+            "ins_api": {
+            "version":"1.0",
+            "type": "cli_show",
+            "chunk": "0",
+            "sid":"1",
+            "input":"show run",
+            "output_format":"json"
+            }
+        }"
+
+
+
 headers = {
   'Content-Type': 'application/json',
   'Authorization': 'Basic YWRtaW46YWRtaW4=',
   'Cookie': 'nxapi_auth=admin:159448523198363176'
 }
 
-response = requests.request("POST", url, headers=headers, data = payload)
+response = requests.POST(url, headers=headers, data = payload)
 
 print(response.text.encode('utf8'))
 
